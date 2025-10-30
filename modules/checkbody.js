4d629695231a0b4442ddd24b7e639e4cdbe62a22
Ben <bquellec@d2a.mc>
2025-10-30 17:08:51 +0100

    ----

--- modules/checkbody.js
+++ modules/checkbody.js
@@ -1,11 +1,3 @@
-// function checkBody(body, keys) {
-//   for (const key of keys) {
-//     if (!body[key] || body[key] === '') {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
 function checkBody(body, keys) {
   let isValid = true;
 
